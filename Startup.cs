3049fc8d027a3dd9a6cad8ec3847794a31931d73
JMelendez <jmelendez@doheny.org>
2021-08-03 22:29:23 -0700

    Addition of css for sticky footer. Addition of dependency injections for rendering while running ISS. deleted privacy and about. delted sytle.css

--- Startup.cs
+++ Startup.cs
@@ -24,6 +24,9 @@ namespace RPClassicalBaptistPress
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddControllersWithViews()
+                    .AddRazorRuntimeCompilation();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
